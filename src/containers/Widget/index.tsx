a7eaf8974784b810871d7a81eeea527f734f4d0b
sajibcse68 <sajibcse68@gmail.com>
2022-06-01 01:59:09 +0600

    Add post lore top section

--- src/containers/Widget/index.tsx
+++ src/containers/Widget/index.tsx
@@ -1,23 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { Provider as Eip1193Provider } from '@web3-react/types';
 import AddLore from 'containers/AddLore';
+import { Theme, ThemeProvider } from 'theme';
+import { WIDGET_MIN_WIDTH } from 'constants/misc';
 
 // import logo from './logo.svg';
-import { Button, Alert } from './widgetStyling';
+import { WidgetWrapper, Button, Alert } from './widgetStyling';
 
 export type WidgetProps = {
   web3Provider?: Eip1193Provider | JsonRpcProvider;
   contractAddress?: string;
   tokenId?: string;
   clientId?: string;
+  width?: string | number;
+  theme?: Theme;
+  className?: string;
 };
 
 export default function Widget(props: WidgetProps) {
-  const { web3Provider, contractAddress, tokenId, clientId } = props;
+  const { web3Provider, contractAddress, tokenId, clientId, theme, className } = props;
   const [isLorePosting, setIsLorePosting] = useState(true);
 
+  const width = useMemo(() => {
+    const { width } = props;
+
+    if (width && width < WIDGET_MIN_WIDTH) {
+      console.warn(`Minimum width is ${WIDGET_MIN_WIDTH}px (you set ${width})`);
+
+      return WIDGET_MIN_WIDTH;
+    }
+
+    return width ?? WIDGET_MIN_WIDTH;
+  }, [props.width]);
+
   const checkRequiredFields = () => {
+    console.log(
+      ' >> web3Provider',
+      web3Provider,
+      ' >> contractAddress: ',
+      contractAddress,
+      '> tokenId: ',
+      tokenId,
+      ' > clientId: ',
+      clientId,
+    );
+
     if (!web3Provider) {
       return <Alert>Please provide the web3Provider!</Alert>;
     }
@@ -26,19 +54,22 @@ export default function Widget(props: WidgetProps) {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {checkRequiredFields()}
-        <h1>Hello Hyype Widget!!</h1>
+    <ThemeProvider theme={theme}>
+      <WidgetWrapper width={width}>
+        <div className="App">
+          <header className="App-header">
+            {checkRequiredFields()}
 
-        {isLorePosting ? (
-          <AddLore />
-        ) : (
-          <Button onClick={() => setIsLorePosting(true)}>Post Lore</Button>
-        )}
+            {isLorePosting ? (
+              <AddLore />
+            ) : (
+              <Button onClick={() => setIsLorePosting(true)}>Post Lore</Button>
+            )}
 
-        {}
-      </header>
-    </div>
+            {}
+          </header>
+        </div>
+      </WidgetWrapper>
+    </ThemeProvider>
   );
 }
--- src/containers/Widget/widgetStyling.ts
+++ src/containers/Widget/widgetStyling.ts
@@ -1,30 +1,56 @@
+import styled from 'styled-components/macro';
 
-import styled from 'styled-components';
+export const WidgetWrapper = styled.div<{ width?: string | number }>`
+  display: flex;
+  flex-direction: row;
+  font-size: 16px;
+  font-variant: none;
+  min-width: 320px;
+  margin: 16px; // delete it
+  padding: 28px 20px;
+
+  width: ${({ width }) => width && (isNaN(Number(width)) ? width : `${width}px`)};
+  background-color: ${({ theme }) => theme.container};
+  border: 1px solid rgba(0, 0, 0, 0.75);
+  border-radius: ${({ theme }) => theme.borderRadius}px;
+  box-sizing: border-box;
+  color: ${({ theme }) => theme.primary};
+  -moz-osx-font-smoothing: grayscale;
+
+  * {
+  box-sizing: border-box;
+  font-family: ${({ theme }) =>
+    typeof theme.fontFamily === 'string' ? theme.fontFamily : theme.fontFamily.font};
+
+  @supports (font-variation-settings: normal) {
+    font-family: ${({ theme }) =>
+      typeof theme.fontFamily === 'string' ? undefined : theme.fontFamily.variable};
+  }
+`;
 
 export const Button = styled.button`
-height: 36px;
-border-radius: 18px;
-font-size: 14px;
-letter-spacing: 0.31px;
-font-weight: 600;
-text-transform: capitalize;
-cursor: pointer;
-padding: 8px 25px;
-line-height: 1;
-display: flex;
--webkit-box-align: center;
-align-items: center;
--webkit-box-pack: center;
-justify-content: center;
-outline: none;
-background-position: center center;
-transition: all 0.4s ease 0s;
-border: 1px solid rgb(255, 129, 98);
-background-color: rgb(255, 129, 98);
-color: white;
+  height: 36px;
+  border-radius: 18px;ß
+  font-size: 14px;
+  letter-spacing: 0.31px;
+  font-weight: 600;
+  text-transform: capitalize;
+  cursor: pointer;
+  padding: 8px 25px;
+  line-height: 1;
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  outline: none;
+  background-position: center center;
+  transition: all 0.4s ease 0s;
+  border: 1px solid rgb(255, 129, 98);
+  background-color: rgb(255, 129, 98);
+  color: white;
 `;
 
 export const Alert = styled.p`
-color: red;
+  color: red;
 `;
-
